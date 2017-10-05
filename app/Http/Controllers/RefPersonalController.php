808a546f26edc73a143e641612583763b5596b03
Ivan Rojo <ivanrojo07@gmail.com>
2017-10-05 16:24:18 -0500

    rutas refpersonales y datos laborales

---
+++ app/Http/Controllers/RefPersonalController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RefPersonal;
+use Illuminate\Http\Request;
+
+class RefPersonalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RefPersonal  $refPersonal
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RefPersonal $refPersonal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RefPersonal  $refPersonal
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RefPersonal $refPersonal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RefPersonal  $refPersonal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RefPersonal $refPersonal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RefPersonal  $refPersonal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RefPersonal $refPersonal)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('personals', 'PersonalController');
 Route::resource('datoslaborales','DatosLabController');
+Route::resource('referenciapersonales','RefPersonalController');
