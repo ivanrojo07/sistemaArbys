1cea78cc6789664db590b548c280c26892c43128
Ivan Rojo <ivanrojo07@gmail.com>
2017-10-18 11:38:43 -0500

    personal-create.php

--- resources/views/personal/create.blade.php
+++ resources/views/personal/create.blade.php
@@ -38,96 +38,96 @@
 
 					<div class="col-md-12 offset-md-2 mt-3" id="permoral" style="display:none;">
 						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-<<<<<<< HEAD
+
 	  						<label class="control-label" for="razonsocial">Razon Social:</label>
 	  						<input type="text" class="form-control" id="razonsocial" name="razonsocial">
-=======
-	  						<label class="control-label" for="nombre">Razon Social:</label>
-	  						<input type="text" class="form-control" id="nombre" name="nombreempr" onkeypress="document.forms.form.nombrepers.value = this.value">
->>>>>>> 048fb935f2d0de00ab53c0dd6730d2470a1223c7
 	  					</div>
-					</div>
-				<div class="col-md-12 offset-md-2 mt-3">
-					<h2><span>Dirección/Domicilio</span></h2>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">	
-						<label class="control-label" for="calle">Calle:</label>
-						<input type="text" class="form-control" id="calle" name="calle">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">	
-						<label class="control-label" for="numext" >Número Exterior:</label>
-						<input type="text" class="form-control" id="numext" name="numext">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<label class="control-label" for="numinter">Número Interior:</label>
-						<input type="text" class="form-control" id="numinter" name="numinter">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<label class="control-label" for="cp">Código Postal:</label>
-						<input type="text" class="form-control" id="cp" name="cp">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<label class="control-label" for="colonia">Colonia:</label>
-						<input type="text" class="form-control" id="colonia" name="colonia">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<label class="control-label" for="municipio">Municipio/Delegación:</label>
-						<input type="text" class="form-control" id="municipio" name="municipio">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<label class="control-label" for="ciudad">Ciudad:</label>
-						<input type="text" class="form-control" id="ciudad" name="ciudad">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<label class="control-label" for="estado">Estado:</label>
-						<input type="text" class="form-control" id="estado" name="estado">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<label class="control-label" for="calle1">Entre calle:</label>
-						<input type="text" class="form-control" id="calle1" name="calle1">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<label class="control-label" for="calle2">Y calle:</label>
-						<input type="text" class="form-control" id="calle2" name="calle2">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<label class="control-label" for="referencia">Referencia:</label>
-						<input type="text" class="form-control" id="referencia" name="referencia">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12" id="cliente" style="display:none;">
-						<label class="control-label" for="recidir">Tiempo recidiendo:</label>
-						<input type="date" class="form-control" id="recidir" name="recidir">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12" id="cliente1" style="display:none;">
-						<label class="control-label" for="vivienda">Tipo de vivienda:</label>
-						<input type="text" class="form-control" id="vivienda" name="vivienda">
+					</div></div>
+				<div class="panel-body">
+					<div class="col-md-12 offset-md-2 mt-3">
+						<h2><span>Dirección/Domicilio</span></h2>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">	
+							<label class="control-label" for="calle">Calle:</label>
+							<input type="text" class="form-control" id="calle" name="calle">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">	
+							<label class="control-label" for="numext" >Número Exterior:</label>
+							<input type="text" class="form-control" id="numext" name="numext">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+							<label class="control-label" for="numinter">Número Interior:</label>
+							<input type="text" class="form-control" id="numinter" name="numinter">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+							<label class="control-label" for="cp">Código Postal:</label>
+							<input type="text" class="form-control" id="cp" name="cp">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+							<label class="control-label" for="colonia">Colonia:</label>
+							<input type="text" class="form-control" id="colonia" name="colonia">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+							<label class="control-label" for="municipio">Municipio/Delegación:</label>
+							<input type="text" class="form-control" id="municipio" name="municipio">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+							<label class="control-label" for="ciudad">Ciudad:</label>
+							<input type="text" class="form-control" id="ciudad" name="ciudad">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+							<label class="control-label" for="estado">Estado:</label>
+							<input type="text" class="form-control" id="estado" name="estado">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+							<label class="control-label" for="calle1">Entre calle:</label>
+							<input type="text" class="form-control" id="calle1" name="calle1">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+							<label class="control-label" for="calle2">Y calle:</label>
+							<input type="text" class="form-control" id="calle2" name="calle2">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+							<label class="control-label" for="referencia">Referencia:</label>
+							<input type="text" class="form-control" id="referencia" name="referencia">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12" id="cliente" style="display:none;">
+							<label class="control-label" for="recidir">Tiempo recidiendo:</label>
+							<input type="date" class="form-control" id="recidir" name="recidir">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12" id="cliente1" style="display:none;">
+							<label class="control-label" for="vivienda">Tipo de vivienda:</label>
+							<input type="text" class="form-control" id="vivienda" name="vivienda">
+						</div>
 					</div>
 				</div>
-				<div class="col-md-12 offset-md-2 mt-3">
-					<h2><span>Datos personales</span></h2>
-					
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<label class="control-label" for="mail">Correo:</label>
-						<input type="email" class="form-control" id="mail" name="mail">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<label class="control-label" for="rfc">RFC:</label>
-						<input type="text" class="form-control" id="rfc" name="rfc">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<label class="control-label" for="telefonofijo">Número de Telefono:</label>
-						<input type="text" class="form-control" id="telefonofijo" name="telefonofijo">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
-						<label class="control-label" for="telefonocel">Número Celular:</label>
-						<input type="text" class="form-control" id="telefonocel" name="telefonocel">
-					</div>
-					<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12" id="cliente2" style="display:none;">
-						<label class="control-label" for="estadocivil">Estado Civil:</label>
-						<select type="select" class="form-control" id="estadocivil" name="estadocivil">
-							<option value="" selected="selected">seleccionar</option>
-    						<option value="Casado">Casado</option>
-    						<option value="Soltero">Soltero</optio>
-    					</select>
+				<div class="panel-body">
+					<div class="col-md-12 offset-md-2 mt-3">
+						<h2><span>Datos personales</span></h2>
+						
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+							<label class="control-label" for="mail">Correo:</label>
+							<input type="email" class="form-control" id="mail" name="mail">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+							<label class="control-label" for="rfc">RFC:</label>
+							<input type="text" class="form-control" id="rfc" name="rfc">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+							<label class="control-label" for="telefonofijo">Número de Telefono:</label>
+							<input type="text" class="form-control" id="telefonofijo" name="telefonofijo">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12">
+							<label class="control-label" for="telefonocel">Número Celular:</label>
+							<input type="text" class="form-control" id="telefonocel" name="telefonocel">
+						</div>
+						<div class="form-group col-lg-3 col-md-3 col-sm-6 col-xs-12" id="cliente2" style="display:none;">
+							<label class="control-label" for="estadocivil">Estado Civil:</label>
+							<select type="select" class="form-control" id="estadocivil" name="estadocivil">
+								<option value="" selected="selected">seleccionar</option>
+	    						<option value="Casado">Casado</option>
+	    						<option value="Soltero">Soltero</optio>
+	    					</select>
+						</div>
 					</div>
 				</div>
   				<button type="submit" class="btn btn-default">Guardar</button>
