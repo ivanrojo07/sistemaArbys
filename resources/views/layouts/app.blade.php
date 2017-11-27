355098031372f1877be1bc85805544eb13e5b02c
Vrios <drmdfoureilu@gmail.com>
2017-11-27 12:18:26 -0600

    ruta2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -110,7 +110,7 @@
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-briefcase" aria-hidden="true"></i> Recursos Humanos <span class="caret"></span> </a>
                         <ul class="dropdown-menu" role="menu">
                             <li>
-                                <a href="/pruebas"><i class="fa fa-plus" aria-hidden="true"></i> Alta</a>
+                                <a href="{{url('pruebas')}}"><i class="fa fa-plus" aria-hidden="true"></i> Alta</a>
                                 {{-- <a href="#"><i class="fa fa-search" aria-hidden="true"></i> Busqueda</a>     --}}
                             </li>                     
                         </ul>
