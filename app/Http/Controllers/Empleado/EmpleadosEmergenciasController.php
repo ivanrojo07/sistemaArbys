7e2a12ff06fe1e131ff4f2f5acdbcf1992aa93d1
Ivan Rojo <ivanrojo07@gmail.com>
2017-11-30 20:51:14 -0600

    Trabajando con EmpleadosEmergenciaController

---
+++ app/Http/Controllers/Empleado/EmpleadosEmergenciasController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Empleado;
+
+use App\Empleado;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class EmpleadosEmergenciasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Empleado $empleado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Empleado $empleado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Empleado $empleado)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Empleado $empleado)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Empleado $empleado)
+    {
+        //
+    }
+}
