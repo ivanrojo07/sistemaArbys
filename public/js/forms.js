eeebd1cd62d92de4ee38c603100fa05a568575c7
Ivan Rojo <ivanrojo07@gmail.com>
2017-12-20 15:12:54 -0600

    function in javascript from edit cliente/personal

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,15 +1,3 @@
-function formulario(elemento){
-            if (elemento.value == "Prospecto") {
-                document.getElementById('cliente').style.display='none';
-                document.getElementById('cliente1').style.display='none';
-                document.getElementById('cliente2').style.display='none';
-            }
-            if (elemento.value == "Cliente") {
-                document.getElementById('cliente').style.display='inline';
-                document.getElementById('cliente1').style.display='inline';
-                document.getElementById('cliente2').style.display='inline';
-            }
-        }
 function persona(elemento){
     if(elemento.value == "Fisica"){
         document.getElementById('perfisica').style.display='inline';
@@ -17,15 +5,89 @@ function persona(elemento){
         document.getElementById('varrfc').pattern="^[A-Za-z]{4}[0-9]{6}[A-Za-z0-9]{3}";
         document.getElementById('varrfc').placeholder="Ingrese 13 caracteres";
         document.getElementById('varrfc').title="Siga el formato 4 letras seguidas por 6 digitos y 3 caracteres";
+        document.getElementById('razonsocial').required=false;
+        document.getElementById('idnombre').required=true;
+        document.getElementById('apellidopaterno').required=true;
     }
-    if(elemento.value =="Moral"){
+    if(elemento.value == "Moral"){
         document.getElementById('perfisica').style.display='none';
         document.getElementById('permoral').style.display='inline';
         document.getElementById('varrfc').pattern="^[A-Za-z]{3}[0-9]{6}[A-Za-z0-9]{3}";
         document.getElementById('varrfc').placeholder="Ingrese 12 caracteres";
         document.getElementById('varrfc').title="Siga el formato 3 letras seguidas por 6 digitos y 3 caracteres";
+        document.getElementById('razonsocial').required=true;
+        document.getElementById('idnombre').required=false;
+        document.getElementById('apellidopaterno').required=false;
     }
 }
+function formulario(elemento){
+    if (elemento.value == "Prospecto") {
+        document.getElementById('cliente').style.display='none';
+        document.getElementById('cliente1').style.display='none';
+        document.getElementById('cliente2').style.display='none';
+        document.getElementById('clienteli1').style.display='none';
+        document.getElementById('clienteli2').style.display='none';
+        document.getElementById('clienteli3').style.display='none';
+        document.getElementById('calle').required=false;
+        document.getElementById('numext').required=false;
+        document.getElementById('cp').required=true;
+        document.getElementById('colonia').required=false;
+        document.getElementById('municipio').required=false;
+        document.getElementById('ciudad').required=false;
+        document.getElementById('estado').required=false;
+    }
+    if (elemento.value == "Cliente") {
+        document.getElementById('cliente').style.display='inline';
+        document.getElementById('cliente1').style.display='inline';
+        document.getElementById('cliente2').style.display='inline';
+        document.getElementById('clienteli1').style.display='';
+        document.getElementById('clienteli2').style.display='';
+        document.getElementById('clienteli3').style.display='';
+        document.getElementById('calle').required=true;
+        document.getElementById('numext').required=true;
+        document.getElementById('cp').required=false;
+        document.getElementById('colonia').required=true;
+        document.getElementById('municipio').required=true;
+        document.getElementById('ciudad').required=true;
+        document.getElementById('estado').required=true;
+        document.getElementById('lbl_numext').value='*numero exterior';
+    }
+}
+$(function(){
+  tipopersona = $('#tipopersona').val();
+  tipo = $('#tipo').val();
+  // console.log(tipo);
+  // var valor = String(tipopersona.value);
+  if(tipopersona == "Moral"){
+    document.getElementById('perfisica').style.display='none';
+    document.getElementById('permoral').style.display='inline';
+    document.getElementById('varrfc').pattern="^[A-Za-z]{3}[0-9]{6}[A-Za-z0-9]{3}";
+    document.getElementById('varrfc').placeholder="Ingrese 12 caracteres";
+    document.getElementById('varrfc').title="Siga el formato 3 letras seguidas por 6 digitos y 3 caracteres";
+    document.getElementById('razonsocial').required=true;
+    document.getElementById('idnombre').required=false;
+    document.getElementById('apellidopaterno').required=false;
+  }
+});
+$(function(){
+    tipo = $('#tipo').val();
+    if(tipo == "Cliente"){
+        document.getElementById('cliente').style.display='inline';
+        document.getElementById('cliente1').style.display='inline';
+        document.getElementById('cliente2').style.display='inline';
+        document.getElementById('clienteli1').style.display='';
+        document.getElementById('clienteli2').style.display='';
+        document.getElementById('clienteli3').style.display='';
+        document.getElementById('calle').required=true;
+        document.getElementById('numext').required=true;
+        document.getElementById('cp').required=false;
+        document.getElementById('colonia').required=true;
+        document.getElementById('municipio').required=true;
+        document.getElementById('ciudad').required=true;
+        document.getElementById('estado').required=true;
+        document.getElementById('lbl_numext').value='*numero exterior';
+    }
+});
 // $('div#tab').remove();
 $("tr").click(function(){
   $('div.persona').hide();
@@ -50,17 +112,6 @@ $('li a').click(function(){
   $('.pestana').hide();
   $(this.getAttribute('href')).show();
 }); 
-$(function(){
-  tipopersona = $('#tipopersona').val();
-  // var valor = String(tipopersona.value);
-  if(tipopersona =="Moral"){
-      document.getElementById('perfisica').style.display='none';
-      document.getElementById('permoral').style.display='inline';
-      document.getElementById('varrfc').pattern="^[A-Za-z]{3}[0-9]{6}[A-Za-z0-9]{3}";
-      document.getElementById('varrfc').placeholder="Ingrese 12 caracteres";
-      document.getElementById('varrfc').title="Siga el formato 3 letras seguidas por 6 digitos y 3 caracteres";
-  }
-});
 
 $(function(){
               $('.dropdown-submenu a.test').on("click", function(e){
