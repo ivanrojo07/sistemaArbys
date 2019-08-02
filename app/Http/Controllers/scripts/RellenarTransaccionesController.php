057dcf7e5cd2cafd70f66191549bcbeb24f25a17
Cristian <cristianguzmansuarez@gmail.com>
2019-08-02 09:41:27 -0500

    Prueba para ver la versión de php

--- app/Http/Controllers/scripts/RellenarTransaccionesController.php
+++ app/Http/Controllers/scripts/RellenarTransaccionesController.php
@@ -15,18 +15,6 @@ class RellenarTransaccionesController extends Controller
 {
     public function status()
     {
-        $oficina = Oficina::where('id', 5)->first();
-
-        if ($oficina) {
-            $gerente = Gerente::where('empleado_id', 7)->first();
-
-            if($gerente){
-                $oficina->gerente_id = $gerente->id;
-            }
-
-        //     $oficina->save();
-        }
-
-        return $oficina;
+        return phpversion();
     }
 }
