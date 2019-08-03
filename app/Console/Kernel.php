10af92e3c91c8dfd6b6b7bc634468baae02e68b1
Cristian <cristianguzmansuarez@gmail.com>
2019-08-03 12:01:07 -0500

    Se automatizó tarea. Cada día 8 de cada mes a las 12:00 se actualizarán los productos con base en el ultimo excel subido

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('update:products')->everyFiveMinutes();
+        $schedule->command('update:products')->monthlyOn(8, '00:00');;
     }
 
     /**
