ad00f7e4a3f7d777ed4dcaf652a003a94911f7aa
Cristian <cristianguzmansuarez@gmail.com>
2019-12-09 19:03:05 -0600

    STORAGE PATH implementado

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,7 +46,7 @@ return [
         'local' => [
             'driver' => 'local',
             //'root' => storage_path('app'),
-            'root' => public_path().'/storage',
+            'root' => env('STORAGE_PATH'),
         ],
 
         'public' => [
